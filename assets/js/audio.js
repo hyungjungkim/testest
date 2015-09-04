74a671e6e0a03bf58cfd221c59226f8f50e54d35
김형중 <gnl4277@gmail.com>
2015-09-04 17:16:51 +0900

    no message

---
+++ assets/js/audio.js
@@ -0,0 +1,11 @@
+<script>
+		$(document).ready(function() {
+			$('#audio-player').mediaelementplayer({
+				alwaysShowControls: true,
+				features: ['playpause','volume','progress'],
+				audioVolume: 'horizontal',
+				audioWidth: 400,
+				audioHeight: 120
+			});
+		});
+</script>
